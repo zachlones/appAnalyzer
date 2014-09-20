722498617240f9e57edaa670270937b9e685aab0
linuxbox <zachlones@gmail.com>
2014-09-20 15:59:02 -0500

    Added a couple of sample apks...

--- analyze.rb
+++ analyze.rb
@@ -1,10 +1,9 @@
 require 'ruby_apk'
 
-apk = Android::Apk.new('10.apk') # set apk file path
+apk = Android::Apk.new('./sampleAPKs/proguardVersion/app-release-unaligned.apk') # set apk file path
+manifest = apk.manifest
+  puts manifest.to_xml
 
 apk.each_file do |name, data|
   puts "#{name}: #{data.size}bytes" # puts file name and data size
-
-manifest = apk.manifest
-  puts manifest.to_xml
 end
